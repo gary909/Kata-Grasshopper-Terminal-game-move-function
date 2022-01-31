b9650a35ac745f10c3bd56ea74d3846f305b7c87
gary <gary.white@lodgit.com>
2022-01-31 13:09:24 +0100

    template

---
+++ index.js
@@ -0,0 +1,7 @@
+function move (position, roll) {
+    // return the new position
+}
+
+console.log(move(0, 4));// return 8
+console.log(move(3, 6));// return 15
+console.log(move(2, 5));// return 12
